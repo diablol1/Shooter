4cf2e1fa7ec1827a949d5fb377f9d15f16508770
diablol1 <patryk.krajewski002@gmail.com>
2017-08-07 14:55:01 +0200

    Add test scene and player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+
+public class PlayerController : MonoBehaviour {
+
+	public float walkSpeed;
+	public float mouseSensitivity;
+
+	public GameObject head;
+
+	CharacterController player;
+
+	void Start () {
+		player = GetComponent<CharacterController> ();
+	}
+
+	void Update () {
+		float verticalMove = Input.GetAxis ("Vertical") * walkSpeed;
+		float horizontalMove = Input.GetAxis ("Horizontal") * walkSpeed;
+
+		float rotX = Input.GetAxis ("Mouse X") * mouseSensitivity;
+		float rotY = Input.GetAxis ("Mouse Y") * mouseSensitivity;
+
+		head.transform.Rotate (-rotY, 0, 0);	
+
+		transform.Rotate (0, rotX, 0);
+
+		Vector3 move = new Vector3 (horizontalMove, 0, verticalMove);
+		move = transform.rotation * move;
+
+		player.Move (move * Time.deltaTime);
+	}
+}
